27cd84babe65022460754c2ff31106a8973df04f
Anielly-g <aniellygon2003@gmail.com>
2024-09-07 21:45:54 -0300

    mudanca curso

--- src/views/CursosView.vue
+++ src/views/CursosView.vue
@@ -7,33 +7,36 @@
             <div class="col">
                 <h2 class="text-secondary">Novo curso</h2>
             </div>
-            <div class="h-100 p-5 bg-light border rounded-3">
-                <div class="mb-3">
-                    <label for="basic-url" class="form-label">Sigla do Curso</label>
-                    <div class="input-group">
-                        <input type="text" class="form-control" id="basic-url"
-                            aria-describedby="basic-addon3 basic-addon4">
+            <form>
+
+                <div class="h-100 p-5 bg-light border rounded-3">
+                    <div class="mb-3">
+                        <label for="basic-url" class="form-label">Sigla do Curso</label>
+                        <div class="input-group">
+                            <input type="text" class="form-control" id="basic-url"
+                            aria-describedby="basic-addon3 basic-addon4" required>
+                        </div>
+                    </div>
+                    <div class="mb-3">
+                        <label for="basic-url" class="form-label">Nome</label>
+                        <div class="input-group">
+                            <input type="text" class="form-control" id="basic-url"
+                            aria-describedby="basic-addon3 basic-addon4" required>
+                        </div>
+                    </div>
+                    <div class="mb-3">
+                        <label for="basic-url" class="form-label">Carga Horária Complementar Exigida</label>
+                        <div class="input-group">
+                            <input type="text" class="form-control" required>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col d-flex justify-content-end">
+                            <button type="submit" class="btn btn-primary text-white">Adicionar</button>
+                        </div>
                     </div>
                 </div>
-                <div class="mb-3">
-                    <label for="basic-url" class="form-label">Nome</label>
-                    <div class="input-group">
-                        <input type="text" class="form-control" id="basic-url"
-                            aria-describedby="basic-addon3 basic-addon4">
-                    </div>
-                </div>
-                <div class="mb-3">
-                    <label for="basic-url" class="form-label">Carga Horária Complementar Exigida</label>
-                    <div class="input-group">
-                        <input type="text" class="form-control">
-                    </div>
-                </div>
-                <div class="row">
-                    <div class="col d-flex justify-content-end">
-                        <button type="submit" class="btn btn-primary text-white">Adicionar</button>
-                    </div>
-                </div>
-            </div>
+            </form>
 
         </div>
     </div>
